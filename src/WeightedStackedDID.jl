ee0334ed1165e212ddb4261b8d92c14eeff79050
eohne <elias.ohneberg@gmail.com>
2024-08-14 10:54:41 +0100

    add option to include subex x id or event time fixed effects

--- src/WeightedStackedDID.jl
+++ src/WeightedStackedDID.jl
@@ -11,7 +11,7 @@ include("ATT.jl");
 include(raw"pre_compile_data.jl");
 
 """
-    stacked_did_reg(data::DataFrame;yvar::Symbol, timeID::Symbol, unitID::Symbol, cohort::Symbol, kappa_pre::Int, kappa_post::Int,x_vars::Vector{Symbol}=Symbol[],fes::Vector{Symbol}=Symbol[],cluster::Union{Nothing,Symbol}=nothing,contrasts::Dict{Symbol, DummyCoding}=Dict{Symbol, DummyCoding}(),multi_thread::Bool=true)
+    stacked_did_reg(data::DataFrame;yvar::Symbol, timeID::Symbol, unitID::Symbol, cohort::Symbol, kappa_pre::Int, kappa_post::Int,x_vars::Vector{Symbol}=Symbol[],fes::Vector{Symbol}=Symbol[],cluster::Union{Nothing,Symbol}=nothing,contrasts::Dict{Symbol, DummyCoding}=Dict{Symbol, DummyCoding}(),multi_thread::Bool=true,inc_sub_exp_fes=false)
 
 Implements the Weighted Stacked Difference-in-Differences of Wing, Freedman, and Hollingsworth (2024).
 
@@ -29,17 +29,21 @@ Implements the Weighted Stacked Difference-in-Differences of Wing, Freedman, and
  * cluster: The cluster variable (currently only supports one). If not supplied it is set at the level of the unitID
  * contrasts: contrasts for DummyVariables. Required if you included a Dummy Variable in x_vars
  * multi_thread: Whether to use multithreading or not in building the stacks
+ * inc_sub_exp_fes :If yes includes sub-experiment-unit-id and sub-experiment-event-time fixed effects 
 
  # Output
 
  FixedEffectModel 
 
 """
-function stacked_did_reg(data::DataFrame;yvar::Symbol, timeID::Symbol, unitID::Symbol, cohort::Symbol, kappa_pre::Int, kappa_post::Int,x_vars::Vector{Symbol}=Symbol[],fes::Vector{Symbol}=Symbol[],cluster::Union{Nothing,Symbol}=nothing,contrasts::Dict{Symbol, DummyCoding}=Dict{Symbol, DummyCoding}(), multi_thread::Bool=true)
+function stacked_did_reg(data::DataFrame;yvar::Symbol, timeID::Symbol, unitID::Symbol, cohort::Symbol, kappa_pre::Int, kappa_post::Int,x_vars::Vector{Symbol}=Symbol[],fes::Vector{Symbol}=Symbol[],cluster::Union{Nothing,Symbol}=nothing,contrasts::Dict{Symbol, DummyCoding}=Dict{Symbol, DummyCoding}(), multi_thread::Bool=true,inc_sub_exp_fes=false)
     dataset = select(data, unique(vcat(yvar,x_vars,fes,timeID,unitID,cohort)))    
     dropmissing!(dataset)
-    
-    _formula=term(yvar)~sum(vcat(term(:event_time_treat),term.(x_vars),fe.(term.(vcat(fes,:treat,:event_time)))))
+    if inc_sub_exp_fes    
+        _formula=term(yvar)~sum(vcat(term(:event_time_treat),term.(x_vars),fe.(term.(vcat(fes,:treat,:event_time,:subex_unitID,:subex_etime)))))
+    else
+        _formula=term(yvar)~sum(vcat(term(:event_time_treat),term.(x_vars),fe.(term.(vcat(fes,:treat,:event_time)))))
+    end
     if isnothing(cluster)
         cluster = unitID
         @info "\nCluster variable was not provided. Clustering will happen on the $(string(unitID)) level!\n"
@@ -93,7 +97,8 @@ function stacked_did_reg(data::DataFrame;yvar::Symbol, timeID::Symbol, unitID::S
         subexpVar = :sub_exp)
         
     transform!(stacked_dtc, [:event_time,:treat] =>  ByRow(*) =>:event_time_treat )
-
+    rtransform!(stacked_dtc, [:sub_exp,unitID] => ByRow((x,y)-> string(x,"_",y))=>:subex_unitID)
+    rtransform!(stacked_dtc, [:sub_exp,:event_time] => ByRow((x,y)-> string(x,"_",y))=>:subex_etime)
     weight_stack = reg(stacked_dtc, _formula, 
                             Vcov.cluster(cluster),
                             weights =:stack_weight, contrasts = merge(Dict(:event_time_treat => DummyCoding(base=-1)),contrasts))
